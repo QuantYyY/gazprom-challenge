ee93640dd05848ce9110dda8a67496871341a472
Maksim Khirug <maxkhirug@gmail.com>
2022-04-07 23:12:51 +0300

    Отредактированы и добавлены стили. Добавлена обработка ошибок и другие фичи для addStudents

--- frontend/src/components/AddStudents/AddStudents.tsx
+++ frontend/src/components/AddStudents/AddStudents.tsx
@@ -4,6 +4,8 @@ import { useForm, SubmitHandler } from "react-hook-form";
 
 import { Text } from "@consta/uikit/Text";
 import { Button } from '@consta/uikit/Button';
+import { Informer } from '@consta/uikit/Informer';
+
 
 import Student from "../../data/data";
 
@@ -13,9 +15,12 @@ const AddStudents: FC<Student> = (props) =>{
     
     const {
         register,
-        formState: { errors },
-        handleSubmit
-    } = useForm<Student>();
+        formState: { errors, isValid },
+        handleSubmit,
+        reset
+    } = useForm<Student>({
+        mode: "onBlur"
+    });
 
     const onSubmit: SubmitHandler<Student> = (data) => {
         axios.post('api/main/', {
@@ -23,35 +28,58 @@ const AddStudents: FC<Student> = (props) =>{
             email: data.email,
             team: data.team,
             link: data.link,
-        })
+        });
+        reset();
+        return <Informer status="success" view="filled" title="Всё хорошо — это 'success'"
+        label="Подходит, чтобы рассказать об успешном результате процесса, в котором сейчас находится пользователь"
+    />
     };
 
     return(
         <>
+        
             <form onSubmit={handleSubmit(onSubmit)} className='form'>
                 <label>
-                   <Text view="primary">ФИО:</Text> 
-                    <input {...register('name')} placeholder="Валентин Петрович" className="label"/>
+                   <Text view="primary" size="xl" className="text">ФИО:</Text> 
+                    <input {...register('name', 
+                    {
+                        required: 'Поле ФИО обязательно к заполнению!',
+                        minLength: {
+                            value: 10,
+                            message: 'Минимум 10 символов в поле ФИО'
+                        }
+                } )} placeholder="Валентин Петрович" className="label"/>
                 </label>
 
                 <label>
-                    <Text view="primary">Электронная почта:</Text> 
-                    <input {...register('email')} placeholder="contest@gazprom.ru" className="label" />
+                    <Text view="primary" size="xl" className="text">Электронная почта:</Text> 
+                    <input {...register('email', {required: 'Поле Электронная почта обязательно к заполнению!'} )} placeholder="contest@gazprom.ru" className="label" />
                 </label>
 
                 <label>
-                    <Text view="primary">Команда:</Text>
+                    <Text view="primary" size="xl" className="text">Команда:</Text>
                     <input {...register('team')} className="label" />
                 </label>
 
                 <label>
-                    <Text view="primary">Telegram:</Text>
-                    <input {...register('link')} placeholder="@gazprom" className="label" />
+                    <Text view="primary" size="xl" className="text">Telegram:</Text>
+                    <input {...register('link', 
+                    {required: 'Поле Telegram обязательно к заполнению!'}
+                     )} placeholder="@gazprom" className="label" />
                 </label>
+
                 
                 <br />
-                <Button type="submit" view="primary" form="default" label='Добавить' className="btn" />
+                <Button type="submit" size="l"  disabled = {!isValid} view="primary" form="default" className="btn" label='Добавить!' />
+
+                <div className="error">
+                    {errors?.name && <p>{errors?.name?.message || "Error!" }</p>}
+                    {errors?.email && <p>{errors?.email?.message || "Error!" }</p>}
+                    {errors?.link && <p>{errors?.link?.message || "Error!" }</p>}
+                </div>
+
             </form>
+            
         </>
     );
 };
--- frontend/src/components/Headers/Headers.tsx
+++ frontend/src/components/Headers/Headers.tsx
@@ -11,7 +11,13 @@ const Item1: menuItems[] = [
 ];
 
 const Item2: menuItems[] = [
-    { label: 'Добавить участников' },
+    { label: 'Добавить' },
+    { active: true },
+
+];
+
+const Item3: menuItems[] = [
+    { label: 'Главная' },
     { active: true },
 
 ];
@@ -28,6 +34,10 @@ const Headers: FC<menuItems> = (props) => {
                         <img src='static/images/GLogo.svg' alt="Logo" className="logo" />
                     </a>
                 </HeaderLogo>
+            </HeaderModule>
+
+            <HeaderModule indent="l">
+                    <a href="/"><HeaderMenu items={Item3} /></a>
             </HeaderModule>
 
             <HeaderModule indent="l">
