c408e5ae53c349ec15a0acccbc3afd7f1cdb38f5
Maksim Khirug <maxkhirug@gmail.com>
2022-04-08 12:46:27 +0300

    Добавлена страница с заданиями. Прописаны 3 задания

--- frontend/src/components/AppRouter/AppRouter.tsx
+++ frontend/src/components/AppRouter/AppRouter.tsx
@@ -4,13 +4,14 @@ import { Routes, Route } from 'react-router-dom';
 import Home from "../MainPages/Home";
 import StudentsList from "../StudentsList";
 import AddStudents from "../AddStudents";
+import Tasks from "../MainPages/Tasks";
 
 const AppRouter: FC = () =>(
     <Routes>
         <Route path="/add" element = {<AddStudents/>}/>
         <Route path="/" element = {<Home/>}/>
         <Route path="/participants" element = {<StudentsList/>}/>
-        
+        <Route path="/tasks" element = {<Tasks/>}/>
     </Routes>
 );
 
